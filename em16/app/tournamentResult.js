9af0ff201a9d5118895e15947f061b5cd5bcb1d5
Robert Painsi <robertpainsi@gmail.com>
2016-06-25 17:44:09 +0200

    Add em16 ⅛final match result Switzerland 4:5 Poland

--- em16/app/tournamentResult.js
+++ em16/app/tournamentResult.js
@@ -1,9 +1,27 @@
 'use strict';
 
 import Tournament from "./tournament";
+import {
+    belgium,
+    croatia,
+    england,
+    france,
+    germany,
+    hungary,
+    iceland,
+    ireland,
+    italy,
+    northernIreland,
+    poland,
+    portugal,
+    slovakia,
+    spain,
+    switzerland,
+    wales
+} from "./nation";
 
 const result = new Tournament();
 
-// result.setScore(switzerland, 1, poland, 2);
+result.setScore(switzerland, 4, poland, 5);
 
 export default result;
