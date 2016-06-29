1dd8b0a7f615822e51e705e3af972a304e1b5313
Robert Painsi <robertpainsi@gmail.com>
2016-06-29 21:01:33 +0200

    Replace flags with flat designed flags

--- em16/app/nation.js
+++ em16/app/nation.js
@@ -19,20 +19,20 @@ class Nation {
     }
 }
 
-export const switzerland = new Nation('Schweiz', 'Switzerland-flag.png');
-export const poland = new Nation('Polen', 'Poland-flag.png');
+export const switzerland = new Nation('Schweiz', 'Switzerland.png');
+export const poland = new Nation('Polen', 'Poland.png');
 export const wales = new Nation('Wales', 'Wales.png');
-export const northernIreland = new Nation('Nordirland', 'Northern-ireland.png');
-export const croatia = new Nation('Kroatien', 'Croatian-flag.png');
-export const portugal = new Nation('Portugal', 'Portugal-flag.png');
-export const france = new Nation('Frankreich', 'France-flag.png');
-export const ireland = new Nation('Irland', 'Ireland-flag.png');
-export const germany = new Nation('Deutschland', 'Germany-flag.png');
-export const slovakia = new Nation('Slovakei', 'Slovakia-flag.png');
-export const hungary = new Nation('Ungarn', 'Hungary-flag.png');
-export const belgium = new Nation('Belgien', 'Belgium-flag.png');
-export const italy = new Nation('Italien', 'Italy-flag.png');
-export const spain = new Nation('Spanien', 'Spain-flag.png');
-export const england = new Nation('England', 'England-flag.png');
-export const iceland = new Nation('Island', 'Iceland-flag.png');
+export const northernIreland = new Nation('Nordirland', 'NorthernIreland.png');
+export const croatia = new Nation('Kroatien', 'Croatia.png');
+export const portugal = new Nation('Portugal', 'Portugal.png');
+export const france = new Nation('Frankreich', 'France.png');
+export const ireland = new Nation('Irland', 'Ireland.png');
+export const germany = new Nation('Deutschland', 'Germany.png');
+export const slovakia = new Nation('Slovakei', 'Slovakia.png');
+export const hungary = new Nation('Ungarn', 'Hungary.png');
+export const belgium = new Nation('Belgien', 'Belgium.png');
+export const italy = new Nation('Italien', 'Italy.png');
+export const spain = new Nation('Spanien', 'Spain.png');
+export const england = new Nation('England', 'England.png');
+export const iceland = new Nation('Island', 'Iceland.png');
 export const unknown = new Nation('Unknown', 'Antarctica.png');
