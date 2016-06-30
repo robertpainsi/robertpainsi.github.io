0530cc59f823270c21e66b6212a568123d629d54
Robert Painsi <robertpainsi@gmail.com>
2016-06-30 23:43:39 +0200

    Add em16 ¼final match result Poland 4:6 Portugal

--- em16/app/tournamentResult.js
+++ em16/app/tournamentResult.js
@@ -31,7 +31,7 @@ result.setScore(hungary, 0, belgium, 4);
 result.setScore(italy, 2, spain, 0);
 result.setScore(england, 1, iceland, 2);
 
-// result.setScore(poland, 0, portugal, 0);
+result.setScore(poland, 4, portugal, 6);
 // result.setScore(wales, 0, belgium, 0);
 // result.setScore(germany, 0, italy, 0);
 // result.setScore(france, 0, iceland, 0);
