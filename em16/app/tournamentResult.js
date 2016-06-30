692bffad898aacc116d649c60e3ad0175f1e4e18
Robert Painsi <robertpainsi@gmail.com>
2016-06-30 23:47:25 +0200

    Set penalty shootout as match result

--- em16/app/tournamentResult.js
+++ em16/app/tournamentResult.js
@@ -22,7 +22,7 @@ import {
 
 const result = new Tournament();
 
-result.setScore(switzerland, 5, poland, 6);
+result.setScore(switzerland, 4, poland, 5);
 result.setScore(wales, 1, northernIreland, 0);
 result.setScore(croatia, 0, portugal, 1);
 result.setScore(france, 2, ireland, 1);
@@ -31,7 +31,7 @@ result.setScore(hungary, 0, belgium, 4);
 result.setScore(italy, 2, spain, 0);
 result.setScore(england, 1, iceland, 2);
 
-result.setScore(poland, 4, portugal, 6);
+result.setScore(poland, 3, portugal, 5);
 // result.setScore(wales, 0, belgium, 0);
 // result.setScore(germany, 0, italy, 0);
 // result.setScore(france, 0, iceland, 0);
