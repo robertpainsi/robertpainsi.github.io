c6b06b2e2dca93d1d237002b08bd9ac629b2a16c
Robert Painsi <robertpainsi@gmail.com>
2016-06-30 16:31:56 +0200

    Add em16 ¼final matches results in javascript (commented out)

--- em16/app/tournamentResult.js
+++ em16/app/tournamentResult.js
@@ -31,4 +31,9 @@ result.setScore(hungary, 0, belgium, 4);
 result.setScore(italy, 2, spain, 0);
 result.setScore(england, 1, iceland, 2);
 
+// result.setScore(poland, 0, portugal, 0);
+// result.setScore(wales, 0, belgium, 0);
+// result.setScore(germany, 0, italy, 0);
+// result.setScore(france, 0, iceland, 0);
+
 export default result;
