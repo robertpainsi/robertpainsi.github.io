683f29c0c069a189097d328d6a95e820e94ea4bb
Robert Painsi <robertpainsi@gmail.com>
2016-07-01 22:51:55 +0200

    Add em16 ¼final match result Wales 3:1 Belgium

--- em16/app/tournamentResult.js
+++ em16/app/tournamentResult.js
@@ -32,7 +32,7 @@ result.setScore(italy, 2, spain, 0);
 result.setScore(england, 1, iceland, 2);
 
 result.setScore(poland, 3, portugal, 5);
-// result.setScore(wales, 0, belgium, 0);
+result.setScore(wales, 3, belgium, 1);
 // result.setScore(germany, 0, italy, 0);
 // result.setScore(france, 0, iceland, 0);
 
