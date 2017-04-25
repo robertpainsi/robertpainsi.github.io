29b811034c553c20275231167c1c005d619be346
Robert Painsi <robertpainsi@gmail.com>
2017-04-25 21:10:33 +0200

    Add local base url (only used for hostname robertpainsi.localhost.io)

--- templates/urls.js
+++ templates/urls.js
@@ -2,6 +2,9 @@
 
 var URLS = (function() {
     var BASE = 'https://raw.githubusercontent.com/robertpainsi/robertpainsi.github.data/master/';
+    if (location.hostname === 'robertpainsi.localhost.io') {
+        BASE = 'http://localhost/robertpainsi.github.data/';
+    }
 
     return {
         BASE: BASE,
